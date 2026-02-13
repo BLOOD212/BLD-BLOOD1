fa0d77f1054b7cf459e451308e7cb49c2ecae849
BLOOD212 <g95829914@gmail.com>
2026-02-13 17:07:49 +0100

    Update stupra.js

--- plugins/stupra.js
+++ plugins/stupra.js
@@ -16,9 +16,9 @@ let handler = async (m, { conn }) => {
   const frasi = [
     `@${sender.split("@")[0]} *stupra e ingravida pesantemente a* @${target.split("@")[0]} `,
     `@${sender.split("@")[0]} *ha spanato l'ano senza pieta a* @${target.split("@")[0]} `,
-    `*Oh*! @${target.split("@")[0]} *ha rapito a*  @${sender.split("@")[0]} *e lo ha portato in un capanno lo ha legatoe iniziato a stuprare hard*  `,
+    `*Oh*! @${target.split("@")[0]} *è stato rapito da*  @${sender.split("@")[0]} *ed è stato portato in un capanno lo ha legato e iniziato a stuprare hard*  `,
     `@${sender.split("@")[0]} *ha tentato di stuprare* @${target.split("@")[0]} *ma le forza dell' ordine u fimmaru*`,
-    `*Tutto il gruppo assiste a* @${sender.split("@")[0]} rapire a @${target.split("@")[0]} *nessuno fa niente*`
+    `*Tutto il gruppo assiste a* @${sender.split("@")[0]} *rapire a* @${target.split("@")[0]} *nessuno fa niente*`
   ];
 
   const fraseRandom = frasi[Math.floor(Math.random() * frasi.length)];
@@ -27,7 +27,7 @@ let handler = async (m, { conn }) => {
   if (!globalThis.abbracciRank[target]) globalThis.abbracciRank[target] = 0;
   globalThis.abbracciRank[target] += 1;
 
-  const testoFinale = `${fraseRandom}\n Stuprir icevuti da @${target.split("@")[0]}: ${globalThis.abbracciRank[target]}`;
+  const testoFinale = `${fraseRandom}\n Stupri ricevuti da @${sender.split("@")[0]}: ${globalThis.abbracciRank[target]}`;
 
   await conn.sendMessage(
     m.chat,
