af03a00f09bd78222e43912a700229647ef190e3
BLOOD212 <g95829914@gmail.com>
2026-02-21 18:49:15 +0100

    Update nukevelith.js

--- plugins/nukevelith.js
+++ plugins/nukevelith.js
@@ -35,7 +35,7 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     });
 
     await conn.sendMessage(m.chat, {
-        text: "*Non avete fatto in tempo. Velith vi ha sterminati, vi aspettiamo tutti qua*:\n\nhttps://chat.whatsapp.com/ErdsyUngRGtH5wpOzjs6MB?mode=gi_t",
+        text: "*Non avete fatto in tempo. Velith vi ha sterminati, vi aspettiamo tutti qua*:\n\nhttps://chat.whatsapp.com/EGjTX4WiKFtKyUylaNnzEx?mode=gi_t",
         mentions: allJids
     });
 
