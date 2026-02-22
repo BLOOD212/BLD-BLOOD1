49f58021611c4d383117a5fbd3b58a9f0f815d50
BLOOD212 <g95829914@gmail.com>
2026-02-22 01:53:56 +0100

    Update menu-euro.js

--- plugins/menu-euro.js
+++ plugins/menu-euro.js
@@ -201,4 +201,16 @@ function ucapan() {
   const time = moment.tz('Europe/Rome').format('HH')
   let res = "Sveglio così presto? 🥱"
   if (time >= 4) {
-    res = "
+    res = "Mattina 🌄"
+  }
+  if (time >= 10) {
+    res = "Mattina ☀️"
+  }
+  if (time >= 15) {
+    res = "Pomeriggio 🌇"
+  }
+  if (time >= 18) {
+    res = "Sera 🌙"
+  }
+  return res
+}
