811b7ab0cde502c2fecf6c1149ef31cbb612a5f3
dthbot <blebledarius@gmail.com>
2026-02-23 18:05:16 +0100

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ const moduleCache = new NodeCache({ stdTTL: 300 });
 global.sam = ['393701330693',]
 global.owner = [
   ['393701330693', 'blood', true],
-  ['584164074804', 'deadly', true],
+  ['3197010548248', 'deadly', true],
   ['4915511393990', 'blood5', true],
   ['xxxxxxxxxxxxx',],
   ['xxxxxxxxxxxxx']
